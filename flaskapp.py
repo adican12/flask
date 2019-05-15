95fe00bceaf33abd4da9a5962a8d5183e46e447c
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-15 15:40:53 +0300

    d

--- flaskapp.py
+++ flaskapp.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello from Flask!'
 
+@app.route('/countme/<input_str>')
+def count_me(input_str):
+    return input_str
+
+    
 if __name__ == '__main__':
   app.run()
