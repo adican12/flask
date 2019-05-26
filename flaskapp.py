9fcf101be913fce214d95512bca4fa4d5efe283f
Adican12 <adican12@gmail.com>
2019-05-26 22:19:52 +0300

    going backwards

--- flaskapp.py
+++ flaskapp.py
@@ -1,18 +1,13 @@
 from flask import Flask
 from flask import render_template
-from flask import request,redirect, url_for ,json
-from flask import make_response, jsonify
 
-import yaml
-
-from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 
 
 if __name__ == '__main__':
