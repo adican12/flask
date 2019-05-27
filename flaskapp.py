7c4bee1f61ce1f564cf5a77d91f9a8d486c0ef3a
Adican12 <adican12@gmail.com>
2019-05-27 15:12:28 +0300

    yaml loader + mysql

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, send_file, request, session, redirect, url_for,make_response
-
 from flask_mysqldb import MySQL
-#
 import yaml
 
 app = Flask(__name__)
+
+db = yaml.load(open('/db.yaml'),Loader=yaml.FullLoader)
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+mysql = MySQL(app)
 
 
 # Route for handling the login page logic
