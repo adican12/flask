0a0b65fc51d188ba163d804beee904e7139e5192
Adican12 <adican12@gmail.com>
2019-05-27 18:56:44 +0300

    start login

--- flaskapp.py
+++ flaskapp.py
@@ -20,14 +20,17 @@ mysql = MySQL(app)
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            # return redirect(url_for('home'))
-            return render_template('/welcome.html')
-    return render_template('login.html', error=error)
+        massage = request.form['username'] + "-----" +  request.form['password']
+        return render_template('/welcome.html',massage)
+    #
+    # error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #         error = 'Invalid Credentials. Please try again.'
+    #     else:
+    #         # return redirect(url_for('home'))
+    # return render_template('login.html', error=error)
 
 
 
