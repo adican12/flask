f7faad998fddaed79b897dea3339bf6fca889193
Adican12 <adican12@gmail.com>
2019-05-27 20:06:24 +0300

    7

--- flaskapp.py
+++ flaskapp.py
@@ -33,19 +33,20 @@ def login():
             # cur.execute("SELECT * FROM `users` WHERE `name`=%s AND `password` =%s" , ( username , password) )
             # cur.execute("SELECT * FROM `users` WHERE `name`=%s " , ( username ) )
             cur.execute("""SELECT * FROM users WHERE name = {}""".format(username))
-
-            rows = cur.fetchall()
-
-            if rows > 0:
-                for r in rows:
-                    massage = massage +"----"
-                    massage = massage + r
-
-                return render_template('login.html',error=massage)
-                # return jsonify(rows)
-            else:
-                massage = massage+ "row < 0"
-                return render_template('login.html',error=massage)
+            massage = " execute"
+            # #
+            # # rows = cur.fetchall()
+            # #
+            # # if rows > 0:
+            # #     for r in rows:
+            # #         massage = massage +"----"
+            # #         massage = massage + r
+            # #
+            # #     return render_template('login.html',error=massage)
+            #     # return jsonify(rows)
+            # else:
+            #     massage = massage+ "row < 0"
+            return render_template('login.html',error=massage)
         except Exception as e:
             e = "error from exception" + e
             return render_template('login.html',error=e)
