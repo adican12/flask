26584ef05796b988a703691605c42097147e057b
Adican12 <adican12@gmail.com>
2019-05-28 17:40:07 +0300

    2

--- flaskapp.py
+++ flaskapp.py
@@ -26,34 +26,31 @@ def login():
         massage = username + "-----------"
         password = request.form['password']
         massage = massage + password
-                return jsonify({
-                "status": "false",
-                "message": "Data fetched fail!",
-                "data": massage})
-        # try:
-        #     cur = mysql.connection.cursor()
-        #
-        #     cur.execute("""SELECT * FROM users WHERE name = {}""".format(username))
-        #
-        #     rows = cur.fetchall()
-        #
-        #     if rows > 0:
-        #         for r in rows:
-        #             massage = massage +"----"
-        #             massage = massage + r
-        #
-        #         return render_template('login.html',error=massage)
-        #         # return jsonify(rows)
-        #     else:
-        #         massage = massage+ "row < 0"
-        #         return render_template('login.html',error=massage)
+
+        try:
+            cur = mysql.connection.cursor()
+
+            cur.execute("""SELECT * FROM users WHERE name = {}""".format(username))
+
+            rows = cur.fetchall()
+
+            if rows > 0:
+                for r in rows:
+                    massage = massage +"----"
+                    massage = massage + r
+
+                return render_template('login.html',error=massage)
+                # return jsonify(rows)
+            else:
+                massage = massage+ "row < 0"
+                return render_template('login.html',error=massage)
 
             # return render_template('welcome.html',massage=massage)
-        # except Exception as e:
-        #         return jsonify({
-        #         "status": "false",
-        #         "message": "Data fetched fail!",
-        #         "data": e})
+        except Exception as e:
+                return jsonify({
+                "status": "false",
+                "message": "Data fetched fails!",
+                "data": users})
 
 
     else:
