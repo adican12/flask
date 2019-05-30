84123101b3aa92906fb78bf90c08a683cf3364e7
Adican12 <adican12@gmail.com>
2019-05-30 20:32:11 +0300

    not ajax

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,17 +1,17 @@
 $(function() {
 
-    $('#btnSignUp').click(function() {
-        alert("form submit");
-        $.ajax({
-            url: '/signup',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-    });
+    // $('#btnSignUp').click(function() {
+    //     alert("form submit");
+    //     $.ajax({
+    //         url: '/signup',
+    //         data: $('form').serialize(),
+    //         type: 'POST',
+    //         success: function(response) {
+    //             console.log(response);
+    //         },
+    //         error: function(error) {
+    //             console.log(error);
+    //         }
+    //     });
+    // });
 });
