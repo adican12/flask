5d7a6bea7c21e69116cd39bae5dd4d5ee95203c7
Adican12 <adican12@gmail.com>
2019-06-03 12:19:50 +0300

    3

--- flaskapp.py
+++ flaskapp.py
@@ -31,14 +31,14 @@ def image():
             # with open(_image, 'rb') as file:
                 # binaryData = file.read()
 
-            image = open(_image)
-            img = image.read()
+            # image = open(_image)
+            # img = image.read()
             # data = binary
-
-            cur = mysql.connection.cursor()
-            query = "INSERT INTO `image` (`name`,`image`) VALUES(%s,%s)"
-            cur.execute(qry, (_image.filename, img ) )
-            cur.commit()
+            #
+            # cur = mysql.connection.cursor()
+            # query = "INSERT INTO `image` (`name`,`image`) VALUES(%s,%s)"
+            # cur.execute(qry, (_image.filename, img ) )
+            # cur.commit()
             return jsonify({"status":success,"data":"works ! "})
         except Exception as e:
             return jsonify({"status":"false",
