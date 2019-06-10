2a935c4d8043d6eb4edba3384cebd50bfcd788ff
Adican12 <adican12@gmail.com>
2019-06-10 13:32:16 +0300

    create bucket

--- flaskapp.py
+++ flaskapp.py
@@ -5,9 +5,17 @@ import yaml
 import os
 import json
 import base64
-
+# Imports the Google Cloud client library
 from google.cloud import storage
 
+# Instantiates a client
+storage_client = storage.Client()
+
+# The name for the new bucket
+bucket_name = 'my-new-bucket'
+
+# Creates the new bucket
+bucket = storage_client.create_bucket(bucket_name)
 app = Flask(__name__)
 
 path = os.path.join('.', os.path.dirname(__file__), 'db.yaml')
