dcc0bdedd1ed927414ac8061f7611e43b2a3ecd6
Adican12 <adican12@gmail.com>
2019-06-10 16:02:02 +0300

    21

--- flaskapp.py
+++ flaskapp.py
@@ -27,19 +27,19 @@ def upload_bucket():
     if request.method == 'POST':
         storage_client = storage.Client()
         image_id = request.files['image_id']
-        # img = image_id.read()
+        img = image_id.read()
 
         bucket_name = "catifi1"
         bucket = storage_client.get_bucket(bucket_name)
         # destination_blob_name="/"
         # blob = bucket.blob(destination_blob_name)
-        # f = image_id.read()
+        f = image_id.read()
 
         blob = bucket.blob(image_id.filename)
-        # blob.upload_from_file(f)
+        blob.upload_from_file(f)
 
-        with open(image_id, 'rb') as f:
-          blob.upload_from_file(f)
+        # with open(image_id, 'rb') as f:
+          # blob.upload_from_file(f)
         # try:
         #     blob.upload_from_file(image_id)
         # except Exception as e:
