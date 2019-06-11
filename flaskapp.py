3896cd05457c8af178b02f17fa105830c39003f3
Adican12 <adican12@gmail.com>
2019-06-11 17:44:06 +0300

    15

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 
-from Flask import Flask, render_template, send_file, request, session, redirect, url_for,make_response, jsonify
+from flask import Flask, render_template, send_file, request, session, redirect, url_for,make_response, jsonify
 # from flask_mysqldb import MySQL
 # import yaml
 # import os
