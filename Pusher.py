50bcd530443937d8f34d1926d27f5c4b0b76493f
Adican12 <adican12@gmail.com>
2019-06-15 17:04:05 +0300

    3

--- Pusher.py
+++ Pusher.py
@@ -9,7 +9,7 @@ proxy_dict = {
           "http"  : "http://127.0.0.1",
 
         }
-push_service = FCMNotification(api_key="AAAAB5JEBKs:APA91bHjEWYSTDRBDkK4wO94bgidJWCfd-TTFEoLRpDfkxcWGkgOiRfuPVZdveYAexQCdxN04eYj1n-PXlmhCxKBchorN1wGIRHB6g4qUNENUPgVYbuvz-dfT8OnB0SXk6tZoljIBQr_", proxy_dict=proxy_dict)
+push_service = FCMNotification(api_key="AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0", proxy_dict=proxy_dict)
 
 # Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
 
@@ -19,7 +19,8 @@ message_body = "Hi john, your customized news for today is ready"
 result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
 
 # Send to multiple devices by passing a list of ids.
-registration_ids = ["ePAszQiwjk8:APA91bEUea36aLpzMvxvZQXH_lDVMtTrkwzz-58if6-8gE2wmACqvNNyFNMt8OZ6rNBGZHSzFRbuN4Fj6wloXUIFpfv2OrV3LmehmcrZTBTr3y_A9vWsy_hjsnBxC6oCnMOjgw5wQaTN"]
+
+registration_ids = ["AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0"]
 message_title = "Cantor the cat"
 message_body = "Hope you're remmber to go out with ligal tonight"
 result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
