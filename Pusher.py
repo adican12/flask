fa628b9ff918ff78208dd93646dcf7216c306515
Adican12 <adican12@gmail.com>
2019-06-16 22:35:26 +0300

    2

--- Pusher.py
+++ Pusher.py
@@ -21,7 +21,8 @@ result = push_service.notify_single_device(registration_id=registration_id, mess
 
 # Send to multiple devices by passing a list of ids.
 
-registration_ids = ["AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0"]
+# registration_ids = ["AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0"]
+registration_ids = ["c0WuKlThF5U:APA91bFUT3AiSm567KGaYgGAGqLQ9UYSsVhzUIQTr63ycsnE1C7ab-Hzlf04Ifiu15T74b27rVcDLbDCpaitLDUacNItXnC00HVJbl4uZad00QPr25tqMCKe5K6x0181ACY1yuTvvJuk"]
 message_title = "Cantor the cat"
 message_body = "Hope you're remmber to go out with ligal tonight"
 result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
