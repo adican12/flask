35ccc446050598e41037e72cd55b50907cd1e4e0
Adican12 <adican12@gmail.com>
2019-06-18 17:25:05 +0300

    6

--- flaskapp.py
+++ flaskapp.py
@@ -64,18 +64,20 @@ mysql = MySQL(app)
 
 
 def push_notf(token_device):
-    push_service = FCMNotification(api_key="<api-key>")
-    # OR initialize with proxies
-    proxy_dict = {
-          "http"  : "http://127.0.0.1",
-        }
-    push_service = FCMNotification(api_key="AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0", proxy_dict=proxy_dict)
-    registration_id = token_device
-    message_title = "Cantor the cat"
-    message_body = "Hope you're remmber to go out with ligal tonight"
-    push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
-# push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
-
+    try:
+        push_service = FCMNotification(api_key="<api-key>")
+        # OR initialize with proxies
+        proxy_dict = {
+              "http"  : "http://127.0.0.1",
+            }
+        push_service = FCMNotification(api_key="AAAABw6N4YM:APA91bGdb1U8UlPAdGJh8fuetr0znEDDdCWIaE0xu7_GBRcg2nFJCW8xtr_-H2mcqPYPCal60wQszlJk_bepAiu1DBCdnQ3NzLGUPk4SPq11fizESsTQbMplpKrAd7aQERdnAf2Bhnf0", proxy_dict=proxy_dict)
+        registration_id = token_device
+        message_title = "Cantor the cat"
+        message_body = "Hope you're remmber to go out with ligal tonight"
+        push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+    # push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+    except:
+        print("push_notf not working", token_device)
 
 
 def handle_token(user_id,token):
