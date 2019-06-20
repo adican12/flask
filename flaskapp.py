f6f1393dd755ab494c21aa124a196a591462ba2c
Adican12 <adican12@gmail.com>
2019-06-20 22:48:58 +0300

    21

--- flaskapp.py
+++ flaskapp.py
@@ -369,9 +369,9 @@ def insert_notf_to_db(list_of_matched):
         cond_query = "SELECT count(*) FROM `notification` WHERE user_id={}  AND  adid={}".format(item[1],item[0])
         cur.execute(cond_query)
         row_count = cur.fetchone()
-        print("row_count: " , row_count)
+        # print("row_count: " , row_count)
         print("row_count: " , row_count["count(*)"] )
-        r =  row_count["count(*)"]
+        r =  int(row_count["count(*)"])
     if r == 0:
         qry = 'INSERT INTO `notification`( `adid`, `user_id`) VALUES( %s , %s )'
         cur.execute(qry,(item[0], item[1]))
