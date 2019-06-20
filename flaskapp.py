15198ffbb34e31857e97a62edff697599d99b4f0
Adican12 <adican12@gmail.com>
2019-06-20 16:01:04 +0300

    fetchall

--- flaskapp.py
+++ flaskapp.py
@@ -29,7 +29,7 @@ def push_user():
             # user_id = str(user_id)
             cur = mysql.connection.cursor()
             cur.execute("""SELECT * from ad WHERE adID in (SELECT adid FROM notification WHERE user_id = {})""".format(user_id))
-            rows = cur.fetchone()
+            rows = cur.fetchall()
             if rows:
                 return jsonify({"status":"true" , "data":rows})
             else:
