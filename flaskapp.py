cc3667ea2846785e95e2604609fcc993c192b221
Adican12 <adican12@gmail.com>
2019-06-28 23:07:15 +0300

    12

--- flaskapp.py
+++ flaskapp.py
@@ -49,7 +49,7 @@ def get_coupnon():
             user_id = request.form['user_id']
             user_id = int(user_id)
             cur = mysql.connection.cursor()
-            cur.execute("""SELECT * from coupon WHERE couponID in (SELECT coupon_id FROM users_coupon WHERE user_id = {})""".format(user_id))
+            cur.execute("""SELECT * from coupon WHERE couponID in (SELECT coupon_id FROM users_coupon WHERE user_id = {} ORDER BY noteid DESC )""".format(user_id))
 
             rows = cur.fetchall()
 
@@ -70,7 +70,7 @@ def get_ads():
             user_id = request.form['user_id']
             # user_id = str(user_id)
             cur = mysql.connection.cursor()
-            cur.execute("""SELECT * from ad WHERE adID in (SELECT adid FROM notification WHERE user_id = {})""".format(user_id))
+            cur.execute("""SELECT * from ad WHERE adID in (SELECT adid FROM notification WHERE user_id = {} ORDER BY noteid DESC )""".format(user_id))
             rows = cur.fetchall()
             if rows:
                 return jsonify({"status":"true" , "data":rows})
